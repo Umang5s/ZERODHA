78a2174e16bdd9e50d60a677c7fd4b8d71f75473
umang patel <129495646+Umang5s@users.noreply.github.com>
2025-01-10 22:26:22 +0530

    Update Login.js

--- dashboard/src/components/Login.js
+++ dashboard/src/components/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://zerodha-backend-6m64.onrender.com/login",
+        "https://zerodha-bakend.onrender.com/login",
         { email, password },
         { withCredentials: true }
       );
