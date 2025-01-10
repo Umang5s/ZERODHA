5f5ce9efc57a6502197ff880e9371a4c481593bd
umang patel <129495646+Umang5s@users.noreply.github.com>
2025-01-10 21:38:02 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("https://zerodha-backend-4u4v.onrender.com/newOrder",{
+    axios.post("https://zerodha-backend-6m64.onrender.com/newOrder",{
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
